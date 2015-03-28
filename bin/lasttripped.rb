a0e26769da69d21328802433758db6a1fb799b25
James S White <james.white@github.com>
2015-03-28 15:40:04 -0500

    lasttripped

--- bin/lasttripped.rb
+++ bin/lasttripped.rb
@@ -6,6 +6,4 @@ require 'veralite'
 # 9 == office door
 vc = VeraLite.client(:bridge => '10.255.0.230')
 last_tripped = vc.bridge.devicebyid(8).state.service['urn:micasaverde-com:serviceId:SecuritySensor1']['LastTrip']
-puts last_tripped
-format = "%d/%m/%Y %H:%M:%S"
-puts "#{DateTime.strptime(last_tripped,"%s").new_offset(-5.0/24)} (#{(Time.now.to_i - last_tripped.to_i)/60} min #{(Time.now.to_i - last_tripped.to_i) % 60}s ago)"
+puts "#{DateTime.strptime(last_tripped,"%s").new_offset(-5.0/24).strftime(format='%H:%M:%S')} (#{(Time.now.to_i - last_tripped.to_i)/60} min #{(Time.now.to_i - last_tripped.to_i) % 60}s ago)"
