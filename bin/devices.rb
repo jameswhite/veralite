bf2c7e4534e7735f96d90a6bcca15654322affa5
James S White <james.white@github.com>
2015-03-28 15:04:27 -0500

    success

--- bin/devices.rb
+++ bin/devices.rb
@@ -2,12 +2,15 @@
 require 'pp'
 $:.unshift File.join(File.dirname(__FILE__), "../lib")
 require 'veralite'
+# 8 == stall door
+# 9 == office door
 while [ 1 ] do
-  vc = VeraLite.client(:bridge => '10.255.0.230')
-  if vc.bridge.devicebyid(9).state.service['urn:micasaverde-com:serviceId:SecuritySensor1']['Tripped'] == 1
-    print "_"
-  else
-    print "#"
-  end
+ vc = VeraLite.client(:bridge => '10.255.0.230')
+ tripped = vc.bridge.devicebyid(8).state.service['urn:micasaverde-com:serviceId:SecuritySensor1']['Tripped'].to_i
+ if tripped == 1
+   print "_"
+ else
+   print "#"
+ end
   sleep(1)
 end
