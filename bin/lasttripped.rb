386a0900bd862e0df6dc15914790b70113f6db26
James S White <james.white@github.com>
2015-03-28 15:50:16 -0500

    time format updates

--- bin/lasttripped.rb
+++ bin/lasttripped.rb
@@ -6,4 +6,8 @@ require 'veralite'
 # 9 == office door
 vc = VeraLite.client(:bridge => '10.255.0.230')
 last_tripped = vc.bridge.devicebyid(8).state.service['urn:micasaverde-com:serviceId:SecuritySensor1']['LastTrip']
-puts "#{DateTime.strptime(last_tripped,"%s").new_offset(-5.0/24).strftime(format='%H:%M:%S')} (#{(Time.now.to_i - last_tripped.to_i)/60} min #{(Time.now.to_i - last_tripped.to_i) % 60}s ago)"
+print "#{DateTime.strptime(last_tripped,"%s").new_offset(-5.0/24).strftime(format='%H:%M:%S')} ("
+print "#{(Time.now.to_i - last_tripped.to_i)/3600}h " if ((Time.now.to_i - last_tripped.to_i)/3600) > 0
+print "#{((Time.now.to_i - last_tripped.to_i)/60) % 60}m " if (((Time.now.to_i - last_tripped.to_i)/60) % 60) > 0
+print "#{(Time.now.to_i - last_tripped.to_i) % 60}s ago"
+puts ")"
