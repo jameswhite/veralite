37c346210d25a25fc68e615f2ce535cce711e964
James S White <james.white@github.com>
2015-03-30 10:04:48 -0500

    more status

--- bin/lasttripped.rb
+++ bin/lasttripped.rb
@@ -5,7 +5,12 @@ require 'veralite'
 # 8 == stall door
 # 9 == office door
 vc = VeraLite.client(:bridge => '10.255.0.230')
+status='closed'
+tripped = vc.bridge.devicebyid(8).state.service['urn:micasaverde-com:serviceId:SecuritySensor1']['Tripped'].to_i
+status = 'open' if tripped
+
 last_tripped = vc.bridge.devicebyid(8).state.service['urn:micasaverde-com:serviceId:SecuritySensor1']['LastTrip']
+print "Currently: [#{status}] Last tripped: "
 print "#{DateTime.strptime(last_tripped,"%s").new_offset(-5.0/24).strftime(format='%H:%M:%S')} ("
 print "#{(Time.now.to_i - last_tripped.to_i)/3600}h " if ((Time.now.to_i - last_tripped.to_i)/3600) > 0
 print "#{((Time.now.to_i - last_tripped.to_i)/60) % 60}m " if (((Time.now.to_i - last_tripped.to_i)/60) % 60) > 0
