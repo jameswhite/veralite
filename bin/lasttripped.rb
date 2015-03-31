90b21980ab20a6e7b44d5f5b5292aac5e5fe4b63
James S White <james.white@github.com>
2015-03-30 22:47:43 -0500

    jpg for closed

--- bin/lasttripped.rb
+++ bin/lasttripped.rb
@@ -7,12 +7,16 @@ require 'veralite'
 vc = VeraLite.client(:bridge => '10.255.0.230')
 status='closed'
 tripped = vc.bridge.devicebyid(8).state.service['urn:micasaverde-com:serviceId:SecuritySensor1']['Tripped'].to_i
-status = 'open' if tripped
 
-last_tripped = vc.bridge.devicebyid(8).state.service['urn:micasaverde-com:serviceId:SecuritySensor1']['LastTrip']
-print "Currently: [#{status}] Last tripped: "
-print "#{DateTime.strptime(last_tripped,"%s").new_offset(-5.0/24).strftime(format='%H:%M:%S')} ("
-print "#{(Time.now.to_i - last_tripped.to_i)/3600}h " if ((Time.now.to_i - last_tripped.to_i)/3600) > 0
-print "#{((Time.now.to_i - last_tripped.to_i)/60) % 60}m " if (((Time.now.to_i - last_tripped.to_i)/60) % 60) > 0
-print "#{(Time.now.to_i - last_tripped.to_i) % 60}s ago"
-puts ")"
+if tripped
+  status = 'open'
+  last_tripped = vc.bridge.devicebyid(8).state.service['urn:micasaverde-com:serviceId:SecuritySensor1']['LastTrip']
+  print "Currently: [#{status}] Last tripped: "
+  print "#{DateTime.strptime(last_tripped,"%s").new_offset(-5.0/24).strftime(format='%H:%M:%S')} ("
+  print "#{(Time.now.to_i - last_tripped.to_i)/3600}h " if ((Time.now.to_i - last_tripped.to_i)/3600) > 0
+  print "#{((Time.now.to_i - last_tripped.to_i)/60) % 60}m " if (((Time.now.to_i - last_tripped.to_i)/60) % 60) > 0
+  print "#{(Time.now.to_i - last_tripped.to_i) % 60}s ago"
+  puts ")"
+else
+  puts "http://i.ytimg.com/vi/kl0V9XHKDXE/maxresdefault.jpg"
+end
